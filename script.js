671d89aa6dfde1f7c28950d45bdd5454ff332a95
cosmincostea21 <cosmincostea0810@gmail.com>
2025-05-19 20:31:34 +0200

    comunidades

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 fetch("provincias.json").then(response=> response.json()).then
 (ArrayComunidades=> {
-    ArrayComunidades.array.forEach(element => {
+    ArrayComunidades.ComunidadesAutonomas.forEach(element => {
         let regiones = document.getElementById("regiones");
         regiones.innerHTML += `<option value="${element.id}">${element.nombre}</option>`;
     });
